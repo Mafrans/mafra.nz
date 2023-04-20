a8f5b78e26943cc2deaa7d8875d0344151331d59
Mafrans <malte.kluft@gmail.com>
2023-04-20 22:13:29 +0200

    feat: basic container width thingy

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import "@fontsource/inter/variable.css";
+  import "../styles/variables.css";
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  :global(body) {
+    font: var(--font-body);
+  }
+
+  main {
+    max-width: var(--container);
+    margin: 200px auto;
+    padding: 0 1rem;
+  }
+</style>
